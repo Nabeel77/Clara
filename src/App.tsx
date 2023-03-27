69f37a472c6a889449038193d01ba42c0dac3207
Nabeel Munir <nabeel.77.dev@Nabeels-MacBook-Air.local>
2023-03-27 09:09:53 +0300

    ci: changed manifest to be compatible with chrome

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold text-blue-200 underline">
-      Hello world!
-    </h1>
+    <div className="w-60 h-60 bg-red-400">
+      <h1 className="text-3xl font-bold text-black underline">
+        Hello world!
+      </h1>
+    </div>  
   );
 }
 
