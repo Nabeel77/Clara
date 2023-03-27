63b5577133d13dfa48973f0c314435cf9db6f324
Nabeel Munir <nabeel.77.dev@Nabeels-MacBook-Air.local>
2023-03-27 11:13:45 +0300

    feat: added messaging body and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Chat } from './components/Chat';
+import { Input } from './components/Input';
 
 const App: React.FC = () => {
+  const extensionDimensions = 'w-112 h-148';
   return (
     <div
-      className="w-112 h-148 bg-gray-dark py-6 relative sm:px-28
-       text-white overflow-hidden flex flex-col justify-between align-middle"
+      className={`${extensionDimensions} bg-gray-dark py-6 relative px-2
+       text-white overflow-hidden flex flex-col justify-between align-middle`}
     >
       {/* header */}
       <div className="uppercase font-bold text-2xl text-center mb-3">
@@ -17,10 +20,14 @@ const App: React.FC = () => {
       <div className="right-gradient z-0 absolute"></div>
 
       {/* body */}
-      <div></div>
+      <div className="h-11/12 overflow-auto w-full max-w-4xl min-w-80 py-8 px-4 self-center">
+        <Chat />
+      </div>
 
       {/* input */}
-      <div></div>
+      <div className="w-full max-w-4xl min-w-80 self-center">
+        <Input />
+      </div>
     </div>
   );
 };
