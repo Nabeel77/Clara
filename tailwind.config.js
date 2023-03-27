bd9daec1aee8e6eb737a65ef1949500747e36af7
Nabeel Munir <nabeel.77.dev@Nabeels-MacBook-Air.local>
2023-03-27 10:21:35 +0300

    feat: added header text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         gray: {
-          dark: "#1A232E"
-        }
-      }
+          dark: '#1A232E',
+        },
+      },
+      width: {
+        112: '28rem',
+      },
+      height: {
+        148: '37rem',
+        172: '48rem',
+      },
     },
   },
   plugins: [],
-}
+};
