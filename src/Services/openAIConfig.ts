01b9897ab01c65b0cf2bea9337844663c3179d28
Nabeel Munir <nabeel.77.dev@Nabeels-MacBook-Air.local>
2023-03-28 18:14:23 +0300

    fix: api key fixes

--- src/Services/openAIConfig.ts
+++ src/Services/openAIConfig.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
   organization: 'org-DV7tpe87HAvnf5ZvYnChS04j',
-  apiKey: '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',
+  apiKey: 'openai-api-secret-here',
 });
 
 export const openai = new OpenAIApi(configuration);
